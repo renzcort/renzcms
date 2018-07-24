f912101f25f832451aa9b9083d8a744f1f8613ae
renzcort <kharenputra@gmail.com>
2018-07-24 17:26:01 +0700

    ilist connect group

--- application/controllers/Sample.php
+++ application/controllers/Sample.php
@@ -6,9 +6,11 @@ class Sample extends CI_Controller {
 	public function __construct()
 	{
 		parent::__construct();
+    $this->load->model('admin/Field_m', 'field');
+
 		//Do your magic here
 		//
-		$this->session_data = $this->auth->logged_in();
+		// $this->session_data = $this->auth->logged_in();
 	}
 
 	public function index()
@@ -20,14 +22,16 @@ class Sample extends CI_Controller {
 
   public function paging() {
     $this->load->library('pagination');
+    $config['base_url'] = base_url('sample/paging');
+    $config['total_rows'] = 200;
+    $config['per_page'] = 20;
+    $this->pagination->initialize($config);
+    echo $this->pagination->create_links();
+  }
 
-$config['base_url'] = base_url('sample/paging');
-$config['total_rows'] = 200;
-$config['per_page'] = 20;
-
-$this->pagination->initialize($config);
-
-echo $this->pagination->create_links();
+  public function listjs(){
+    $data['field'] = $this->field->getAll();
+    $this->load->view('test', $data);
   }
 }
 
--- application/views/admin/general/group/index1.php
+++ application/views/admin/general/group/index1.php
@@ -25,7 +25,49 @@
   </div>
   <?php } ?>
   <div class="row">
-    <div class="col-xs-12">
+    <div class="col-xs-6">
+      <div class="box box-primary">
+        <div class="box-header with-border">
+          <!-- <h3 class="box-title"><?php echo $title; ?></h3> -->
+        </div>
+        <div class="box-body">
+        <?php $attributes = array('role' => 'form') ?>
+          <?php echo form_open_multipart(base_url($action).'/create', $attributes); ?>
+          <input type="hidden" class="form-control" name="name" value="entries">
+          <!-- select -->
+          <div class="form-group">
+            <label>Section</label>
+            <select class="form-control" name="section">
+              <option value="0">None</option>
+              <?php foreach ($getAllSection as $key) { ?>
+              <option value="<?php echo $key->id ?>"><?php echo $key->name ?></option>
+              <?php } ?>
+            </select>
+          </div>
+          <!-- select -->
+          <div class="form-group">
+            <label>Entries</label>
+            <select class="form-control" name="field">
+              <option value="0">None</option>
+              <?php foreach ($getAllField as $key) { ?>
+              <option value="<?php echo $key->id ?>"><?php echo $key->name ?></option>
+              <?php } ?>
+            </select>
+          </div>
+          <!-- select -->
+          <!-- /.box-body -->
+          <div class="box-footer">
+            <button type="submit" name="submit" value="submit" class="btn btn-primary">Submit</button>
+          </div>
+          <!-- </form> -->
+        <?php echo form_close(); ?>
+        </div>
+      </div>
+      <!-- /.box -->
+      
+    </div>
+    <!-- /.col -->
+    <div class="col-xs-6">
       <div class="box">
         <div class="box-header">
           <h3 class="box-title"><?php echo $title ?></h3>
@@ -37,23 +79,33 @@
               <tr>
                 <th>No</th>
                 <th>Section</th>
-                <th>handle</th>
-                <th>Type</th>
-                <th>URL Format</th>
+                <th>Field</th>
                 <th>Action</th>
               </tr>
             </thead>
             <tbody class="text-left">
               <?php if($total_rows) {?>
               <?php $i = 0 ?>
-              <?php foreach ($getAllSection as $key ) {?>
+              <?php foreach ($getAll as $key ) {?>
               <tr>
                 <input type="hidden" name="id" value="<?php echo $key->id; ?>">
                 <td><?php echo $i = $i + 1; ?></td>
-                <td><?php echo $key->name ?></td>
-                <td></td>
-                <td><?php echo $key->type ?></td>
-                <td><?php echo $key->slug ?></td>
+                <?php foreach ($getAllSection as $value) {
+                    if ($value->id == $key->id_section) {
+                ?>
+                  <td><?php echo $value->name ?></td>
+                <?php
+                    }
+                } 
+                ?>
+                <?php foreach ($getAllField as $value) {
+                    if ($value->id == $key->id_field) {
+                ?>
+                  <td><?php echo $value->name ?></td>
+                <?php
+                    }
+                } 
+                ?>
                 <td colspan="2" class="action">
                   <a class="btn btn-sm btn-info" href="<?php echo base_url($action.'/edit/').$key->id ?>" title="Edit"><i class="fa fa-pencil"></i></a>
                   <a class="btn btn-sm btn-danger deleteUser" href="<?php echo base_url($action.'/delete/').$key->id ?>" data-userid="2" title="Delete"><i class="fa fa-trash"></i></a>
